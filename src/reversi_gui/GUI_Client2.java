d24caa1943bc9a11b23d6b37a5ad5d0e85fb4459
Kevin <kjb2503@rit.edu>
2017-11-04 13:23:44 -0400

    Made some more progress in terms of the items that are required to build our GUI.

--- src/reversi_gui/GUI_Client2.java
+++ src/reversi_gui/GUI_Client2.java
@@ -10,7 +10,6 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
-import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 import reversi.ReversiException;
@@ -40,9 +39,16 @@ public class GUI_Client2 extends Application implements Observer {
      */
     private Map< String, String > params = null;
 
+    /**
+     *
+     */
     private Label remaining = new Label("GAME INITIALIZING...");
     private Label move = new Label("WAITING");
     private Label status = new Label("RUNNING");
+
+    private Image empty = new Image(getClass().getResourceAsStream("empty.jpg"));
+    private Image p1 = new Image(getClass().getResourceAsStream("othelloP1.jpg"));
+    private Image p2 = new Image(getClass().getResourceAsStream("othelloP2.jpg"));
 
     /**
      * Look up a named command line parameter (format "--name=value")
@@ -65,7 +71,7 @@ public class GUI_Client2 extends Application implements Observer {
     }
 
     /**
-     *
+     * Initializes the client before a build of the game board.
      */
     @Override
     public void init()
@@ -93,95 +99,139 @@ public class GUI_Client2 extends Application implements Observer {
     private void refresh()
     {
         System.out.println("refreshing");
-//        if ( !this.model.isMyTurn() ) {
-//            this.userOut.println( this.model );
-//            this.userOut.println( this.model.getMovesLeft() + " moves left." );
-//            Board.Status status = this.model.getStatus();
-//            switch ( status )
-//            {
-//                case ERROR:
-//                    //this.userOut.println( status );
-//                    this.endGame();
-//                    break;
-//                case I_WON:
-//                    this.userOut.println( "You won. Yay!" );
-//                    this.endGame();
-//                    break;
-//                case I_LOST:
-//                    this.userOut.println( "You lost. Boo!" );
-//                    this.endGame();
-//                    break;
-//                case TIE:
-//                    this.userOut.println( "Tie game. Meh." );
-//                    this.endGame();
-//                    break;
-//                default:
-//                    this.userOut.println();
-//            }
-//        }
-//        else {
-//            boolean done = false;
-//            do {
-//                // this.userOut.print("type move as row◻︎column: ");
+        if ( !this.model.isMyTurn() ) {
+
+            move.setText(this.model.getMovesLeft() + " moves left.");
+
+            Board.Status status = this.model.getStatus();
+            switch (status)
+            {
+                case ERROR:
+                    // change /status/ label to say there was an error
+                    this.endGame();
+                    break;
+                case I_WON:
+                    // change the /status/ label to say: Game over. You won!
+                    this.endGame();
+                    break;
+                case I_LOST:
+                    // change the /status/ label to say: Game over. You lost!
+                    this.endGame();
+                    break;
+                case TIE:
+                    // change the /status/ label to say: Game over. You tied.
+                    this.endGame();
+                    break;
+            }
+        }
+        else {
+            boolean stillMoving = true;
+            while (stillMoving) {
+//                this.userOut.print("type move as row◻︎column: ");
 //                this.userOut.flush();
-//                int row = this.userIn.nextInt();
-//                int col = this.userIn.nextInt();
-//                if (this.model.isValidMove(row, col)) {
-//                    this.userOut.println(this.userIn.nextLine());
-//                    this.serverConn.sendMove(row, col);
-//                    done = true;
-//                }
-//            } while (!done);
-//        }
+                int row = 0; //this.userIn.nextInt();
+                int col = 1; //this.userIn.nextInt();
+                if (this.model.isValidMove(row, col)) {
+                    this.serverConn.sendMove(row, col);
+                    stillMoving = false;
+                }
+            }
+        }
     }
 
     /**
      *
-     * @param t
-     * @param o
+     * @param observable
+     * @param object
      */
-    public void update(Observable t, Object o)
+    public void update(Observable observable, Object object)
     {
-        assert t == this.model: "Update from non-model Observable";
+        // if observable not our model, game crashes with this error
+        assert observable == this.model: "Update from non-model Observable";
 
+        // refresh our game since it is our turn
         this.refresh();
     }
 
     public void start( Stage mainStage ) {
         BorderPane pane = new BorderPane();
         GridPane gp = new GridPane();
-        // sets the alignments of the labels
-        remaining.setTextAlignment(TextAlignment.LEFT);
-        move.setTextAlignment(TextAlignment.CENTER);
-        status.setTextAlignment(TextAlignment.RIGHT);
-        // adds the labels to an HBox
-        FlowPane labels = new FlowPane(remaining, move, status);
 
-
-        for(int i = 0; i <= 3; i++)
+        // sets in place all of the buttons that we need
+        for(int i = 0; i < this.model.getNCols(); i++)
         {
-            for(int j = 0; j <= 3; j++)
+            for(int j = 0; j < this.model.getNRows(); j++)
             {
-                String id = i + ", " + j;
-                Image imageDecline = new Image(getClass().getResourceAsStream("empty.jpg"));
-                Button button = new Button();
-                button.setOnAction( (EventAction) -> System.out.println(id) );
-                button.setGraphic(new ImageView(imageDecline));
-                gp.add(button, i, j);
+                // row and col of the button
+                int col = i;
+                int row = j;
+
+                // makes a new button
+                Button btn = new Button(i + " " + j);
+                // makes the graphic for the button
+                btn.setGraphic(new ImageView(empty));
+                // adds an event to the button
+                btn.setOnAction( (EventAction) -> this.serverConn.sendMove(row,col) );
+                // adds the button to the GridPane
+                gp.add(btn, i, j);
             }
         }
 
         // sets the GridPane to the bottom of the BorderPane
         pane.setCenter(gp);
+
+
+
+        // makes two spacers for the bottom HBox
+        Region spacer1 = new Region();
+        Region spacer2 = new Region();
+
+        // sets the attributes of the spacers
+        HBox.setHgrow(spacer1, Priority.ALWAYS);
+        HBox.setHgrow(spacer2, Priority.ALWAYS);
+
+        // makes a new labels HBox
+        HBox labels = new HBox(remaining, spacer1, move, spacer2, status);
+
         // sets the status HBox to the bottom of the BorderPane
         pane.setBottom(labels);
+
+
 
         // sets the scene as a new scene of the pane
         mainStage.setScene(new Scene(pane));
 
         // we have now completed building our GUI, we can show
         mainStage.show();
+
+        //this.beginGame();
     }
+
+    private synchronized void beginGame()
+    {
+        // add ourselves as an observer
+        this.model.addObserver(this);
+
+        // refreshes our game
+        this.refresh();
+
+        while ( this.model.getStatus() == Board.Status.NOT_OVER )
+        {
+            try {
+                this.wait();
+            }
+            catch( InterruptedException ie ) { /*sauce*/ }
+        }
+    }
+
+    /**
+     * Ends the game.
+     */
+    private synchronized void endGame()
+    {
+        this.notify();
+    }
+
 
     /**
      * GUI is closing, so close the network connection. Server will

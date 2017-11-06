e3dd0f8343f03b4483c57b34c675f459ecd2d213
Kevin <kjb2503@rit.edu>
2017-11-06 09:46:42 -0500

    Made a neato mouse-in/out feature so that users would clearly know which piece they are currently hovering over.

--- src/reversi_gui/GUI_Client2.java
+++ src/reversi_gui/GUI_Client2.java
@@ -3,10 +3,12 @@ package reversi_gui;
 import java.util.*;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
@@ -39,7 +41,7 @@ public class GUI_Client2 extends Application implements Observer
     /** A Label that tells the user whose move it currently is. */
     private Label currentMove = new Label();
     /** A Label that tells the user the status of the game (USUALLY ON). */
-    private Label gameStatus = new Label("RUNNING");
+    private Label gameStatus = new Label("Running");
 
     /** The GridPane that our tiles will be held in. Consider this the board game. */
     private GridPane gp;
@@ -98,7 +100,7 @@ public class GUI_Client2 extends Application implements Observer
     public void start( Stage mainStage )
     {
         // our main viewport
-        BorderPane pane = new BorderPane();
+        BorderPane rootPane = new BorderPane();
 
         // builds the button plane
         this.gp = buildButtonGridPane(this.model.getNRows(), this.model.getNCols());
@@ -111,28 +113,29 @@ public class GUI_Client2 extends Application implements Observer
         disableSpaces();
 
         // sets the center of our BorderPane to be the GridPane
-        pane.setCenter(this.gp);
+        rootPane.setCenter(this.gp);
 
         // builds and sets our bottom HBox to the necessary labels (don't need to keep the HBox accessible)
-        pane.setBottom(buildBottomLabelHBox());
+        rootPane.setBottom(buildBottomLabelHBox());
 
         // builds and sets our top HBox to the necessary labels
-        pane.setTop(buildTopLabelHBox());
+        rootPane.setTop(buildTopLabelHBox());
 
         // refreshes as soon as we begin so that the labels say the correct things
         refresh();
 
+        // add ourselves to the model's observer list
+        this.model.addObserver(this);
+
 
         // now we need to finalize the creation of the stage and scene.
-        // sets the scene as a new scene of the pane
-        mainStage.setScene(new Scene(pane));
+        mainStage.setScene(new Scene(rootPane));
         // sets the title of the window to be Reversi
         mainStage.setTitle("Reversi");
         // adds an icon to the window
         mainStage.getIcons().add(new Image(getClass().getResourceAsStream("othello2.png")));
-
-        // add ourselves to the model's observer list
-        this.model.addObserver(this);
+        // sets it so that the window can't be resized (makes it so that everything is right)
+        mainStage.setResizable(false);
 
         // we have now completed building our GUI, we can show it
         mainStage.show();
@@ -140,8 +143,7 @@ public class GUI_Client2 extends Application implements Observer
     }
 
     /**
-     * GUI is closing, so close the network connection. Server will
-     * get the message.
+     * GUI is closing, so close the network connection. Server will get the message.
      */
     @Override
     public void stop() throws Exception
@@ -160,6 +162,7 @@ public class GUI_Client2 extends Application implements Observer
     {
         // an empty GridPane that will eventually be returned
         GridPane gp = new GridPane();
+
         // sets in place all of the buttons that we need
         for(int col = 0; col < totalCols; ++col)
         {
@@ -171,8 +174,15 @@ public class GUI_Client2 extends Application implements Observer
                 btn.setId(col + " " + row);
                 // makes the graphic for the middle pieces their corresponding players
                 btn.setGraphic(new ImageView(empty));
-                // adds an event to the button
+                // sets the buttons so they have rectangular corners (rather than rounded, makes it look uniform)
+                btn.setStyle("-fx-background-radius: 0em; ");
+                // adds an event to the button so that a move is checked
                 btn.setOnMouseClicked( (event) -> checkMove(btn) );
+                // adds an event so that on mouseover, the piece brightens a bit
+                btn.setOnMouseEntered( (event) -> mouseEnter(btn));
+                // adds an event so that on mouseexit, the piece loses its brightness
+                btn.setOnMouseExited( (event) -> mouseExit(btn));
+
                 // adds the button to the GridPane
                 gp.add(btn, col, row);
             }
@@ -200,6 +210,7 @@ public class GUI_Client2 extends Application implements Observer
         // sets the text to the correct size in the label
         currentMove.setFont(new Font(30));
 
+        // returns a new HBox (we don't need to specifically save it for access)
         return new HBox(spacer1, currentMove, spacer2);
     }
 
@@ -224,7 +235,7 @@ public class GUI_Client2 extends Application implements Observer
         movesRemaining.setFont(new Font(30));
         gameStatus.setFont(new Font(30));
 
-        // creates and returns a new labels HBox
+        // returns a new HBox (we don't need to specifically save it for access)
         return new HBox(spacer1, movesRemaining, spacer2, gameStatus, spacer3);
     }
 
@@ -262,22 +273,9 @@ public class GUI_Client2 extends Application implements Observer
         // if the game is over, we need to deal with that now
         if(this.model.getStatus() != Board.Status.NOT_OVER)
         {
-            // determine which message we need to show the user because we're at the end case
-            switch (this.model.getStatus())
-            {
-                case ERROR:
-                    gameOver("ERROR");
-                    break;
-                case I_WON:
-                    gameOver("Game over. You won!");
-                    break;
-                case I_LOST:
-                    gameOver("Game over. You lost!");
-                    break;
-                case TIE:
-                    gameOver("Game over. You tied!");
-                    break;
-            }
+            // run the gameOver(Board.Status) method to finalize our finished game (i.e. change the Labels to say the
+            // correct stuff).
+            gameOver(this.model.getStatus());
         }
     }
 
@@ -307,12 +305,8 @@ public class GUI_Client2 extends Application implements Observer
      */
     private void checkMove(Button btn)
     {
-        // splits the location of the button based on the ID of it
-        String [] location = btn.getId().split(" ");
-
-        // sets the button's row and column
-        int row = Integer.parseInt(location[1]);
-        int col = Integer.parseInt(location[0]);
+        int row = getButtonRow(btn);
+        int col = getButtonColumn(btn);
 
         // if its my turn
         if(this.model.isMyTurn())
@@ -340,6 +334,98 @@ public class GUI_Client2 extends Application implements Observer
     }
 
     /**
+     * Creates the effect of the button being slightly brighter when the mouse enters its field.
+     *
+     * @param btn The button in which should now be a bit brighter.
+     */
+    private void mouseEnter(Button btn)
+    {
+        // set a filter to make the image 20% brighter (so we know which one the mouse is over)
+        ColorAdjust bright = new ColorAdjust();
+        bright.setBrightness(0.3);
+
+        // determine the image we need
+        int row = getButtonRow(btn);
+        int col = getButtonColumn(btn);
+
+        // creates a new button ImageView
+        ImageView newBtn = getImageView(row, col);
+
+        // set the effect to the new button
+        newBtn.setEffect(bright);
+        // set the image we have just created
+        javafx.application.Platform.runLater(() ->
+                btn.setGraphic(newBtn));
+    }
+
+    /**
+     * Removes any sort of brightness on a button once the mouse exits its field.
+     *
+     * @param btn The button to remove the brightness from.
+     */
+    private void mouseExit(Button btn)
+    {
+        // determine the image we need
+        int row = getButtonRow(btn);
+        int col = getButtonColumn(btn);
+
+        // creates a new button ImageView
+        ImageView btnReset = getImageView(row, col);
+
+        javafx.application.Platform.runLater(() ->
+                btn.setGraphic(btnReset));
+    }
+
+    /**
+     * Returns a new ImageView based on the player at row, col.
+     *
+     * @param row The row to search in.
+     * @param col The column to search in.
+     *
+     * @return A new ImageView of the correct type.
+     */
+    private ImageView getImageView(int row, int col)
+    {
+
+        switch(this.model.getContents(row, col))
+        {
+            case PLAYER_ONE:
+                return new ImageView(p1);
+            case PLAYER_TWO:
+                return new ImageView(p2);
+            default:
+                // NONE by default
+                return new ImageView(empty);
+        }
+    }
+
+    /**
+     * Gets the row of a button in the GridPane.
+     *
+     * @param btn The button we are searching for.
+     *
+     * @return The row that the button btn is found in.
+     */
+    private int getButtonRow(Button btn)
+    {
+        // splits the location of the button based on the ID of it
+        return Integer.parseInt(btn.getId().split(" ")[1]);
+    }
+
+    /**
+     * Gets the column of a button in the GridPane.
+     *
+     * @param btn The button we are searching for.
+     *
+     * @return The column that the button btn is found in.
+     */
+    private int getButtonColumn(Button btn)
+    {
+        // gets the column the button is in
+        return Integer.parseInt(btn.getId().split(" ")[0]);
+    }
+
+    /**
      * Updates the label that contains the number of moves remaining.
      */
     private void updateMovesLeft()
@@ -347,7 +433,7 @@ public class GUI_Client2 extends Application implements Observer
         // gets the number of moves left (used for ternary to get plurality correct)
         int movesLeft = this.model.getMovesLeft();
         javafx.application.Platform.runLater(() ->
-                movesRemaining.setText(movesLeft + (movesLeft != 1 ? " MOVES": " MOVE")));
+                movesRemaining.setText(movesLeft + (movesLeft != 1 ? " MOVES LEFT": " MOVE LEFT")));
     }
 
     /**
@@ -359,15 +445,14 @@ public class GUI_Client2 extends Application implements Observer
         // needed every time we refresh (game over or not)
         for(Node child: this.gp.getChildren())
         {
-            // gets the row and the column of the child
-            int row = GridPane.getRowIndex(child);
-            int col = GridPane.getColumnIndex(child);
-
             // the only children are buttons, but just as a fail-safe we check that here
             if(child instanceof Button)
             {
-                //cast c as a Button
+                //cast child as a Button
                 Button btn = (Button) child;
+
+                int row = getButtonRow(btn);
+                int col = getButtonColumn(btn);
 
                 // we only need to worry about changing the images to p1 or p2, nothing causes the plane to
                 // switch a piece from occupied to empty.
@@ -384,16 +469,54 @@ public class GUI_Client2 extends Application implements Observer
     /**
      * This method is called when the game is over. It updates all of the labels and then nothing else happens.
      *
-     * @param msg The message that needs to be printed on the top label (win, loss, tie, error).
+     * @param status The status that was passed to the client as the way the game ended.
      */
-    private void gameOver(String msg)
+    private void gameOver(Board.Status status)
     {
-        // set the message on the gameStatus
+        // the message that will be put in the place of the currentMove label (the (not) your turn thing)
+        String msg;
+        // the color that the currentMove label will be changed to
+        Color color;
+
+        // determine what the status was
+        switch(status)
+        {
+            case I_WON:
+                // the game was a win
+                msg = "Game over. You won!";
+                color = Color.web("#24b749");
+                break;
+            case I_LOST:
+                // the game was a loss
+                msg = "Game over. You lost!";
+                color = Color.web("#b73131");
+                break;
+            case TIE:
+                // the game was a tie
+                msg = "Game over. You tied!";
+                color = Color.web("#2689af");
+                break;
+            default:
+                // the default is an error
+                msg = "AN ERROR OCCURRED!";
+                color = Color.web("#b70000");
+        }
+
+        // sets the message on the currentMove
         javafx.application.Platform.runLater(() ->
                 currentMove.setText(msg));
-        // set the currentMove to say that the game is over
+
+        // sets the color of the currentMove to be the right color
+        javafx.application.Platform.runLater(() ->
+                currentMove.setTextFill(color));
+
+        // sets the gameStatus to say that the game is over
         javafx.application.Platform.runLater(() ->
                 gameStatus.setText("Stopped"));
+
+        // sets the color of gameStatus to be red so that the user knows the game was stop
+        javafx.application.Platform.runLater(() ->
+                gameStatus.setTextFill(Color.web("#b73131")));
     }
 
     /**
@@ -417,16 +540,22 @@ public class GUI_Client2 extends Application implements Observer
         // goes through each child in the GridPane
         for(Node child: this.gp.getChildren())
         {
-            // gets the row and the column of this specific child
-            int row = GridPane.getRowIndex(child);
-            int col = GridPane.getColumnIndex(child);
-
-            // if its a valid spot to place the object, or it has anything other than a NONE move, it is enabled
-            if(this.model.isValidMove(row, col) || this.model.getContents(row, col) != Board.Move.NONE)
+            // just a check to make sure the button is still a button (it's just a fail-safe measure)
+            if(child instanceof Button)
             {
-                // this enables it when the scheduler wants to run it
-                javafx.application.Platform.runLater(() ->
-                        child.setDisable(false));
+                // cast child as a button
+                Button btn = (Button) child;
+
+                int row = getButtonRow(btn);
+                int col = getButtonColumn(btn);
+
+                // if its a valid spot to place the object, or it has anything other than a NONE move, it is enabled
+                if(this.model.isValidMove(row, col) || this.model.getContents(row, col) != Board.Move.NONE)
+                {
+                    // this enables it when the scheduler wants to run it
+                    javafx.application.Platform.runLater(() ->
+                            child.setDisable(false));
+                }
             }
         }
     }
